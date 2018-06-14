36b3e855f6ad2a610556605ac7e3be78331fbd7e
Vicki Hamm <vicki.cello@gmail.com>
2018-06-14 19:26:55 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,6 +4,17 @@ class TicTacToe
     @board = board || Array.new(9, " ")
   end
 
+  WIN_COMBINATIONS [
+    [0,1,2]
+    [3,4,5]
+    [6,7,8]
+    [0,3,6]
+    [1,4,7]
+    [2,5,8]
+    [0,4,8]
+    [6,4,2]
+  ]
+  
   def current_player
   turn_count % 2 == 0 ? "X" : "O"
   end
@@ -19,3 +30,5 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
+
+  
